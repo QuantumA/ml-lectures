ce66419b5b70c7e96f846e16a759b4f65de454cc
QuantumA <a.cperezdev@gmail.com>
2022-09-13 19:37:01 +0200

    Minor changes

--- classifiers/percetron.py
+++ classifiers/percetron.py
@@ -4,12 +4,10 @@ import numpy as np
 class Perceptron:
     weights = None
     bias = None
-    errors = []
 
     def __init__(self, eta=0.01, random_state=1):
         self.eta = eta
         self.random_state = random_state
-        self.errors = []
 
     def fit(self, features, labels, epochs):
 
@@ -17,15 +15,12 @@ class Perceptron:
         self.weights = initializer.normal(loc=0.0, scale=0.01, size=features.shape[1])
         self.bias = np.float_(0.)
 
+        update = None
         for epoch in range(epochs):
-            errors = 0
             for x_i, y_i in zip(features, labels):
                 update = (y_i - self.predict(features=x_i)) * self.eta
                 self.weights += update * x_i
                 self.bias += update
-
-                errors += int(update != 0.0)
-            self.errors.append(errors)
 
         return self
 
